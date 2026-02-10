be85cc8ef21a12ac66cd31bc73987596aee29ab4
KevinU <kevinuribe114@gmail.com>
2026-02-10 13:04:34 -0500

    product controller

--- src/modules/products/product.controller.js
+++ src/modules/products/product.controller.js
@@ -2,8 +2,17 @@ import * as productService from './product.service';
 
 export const getProducts = async(req, res) => {
     try {
-        
+        const products = await productService.getAllProducts();
+        res.status(200).json(products);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-} 
+}; 
+
+export const getProduct = async(req, res) => {
+    try {
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({ error: error.message });        
+    }
+}
